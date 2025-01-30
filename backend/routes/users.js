e6c3a8d774efd63a359047e4698d61ee19ece3e3
Arjun <bijuarjun45@gmail.com>
2025-01-31 00:00:36 +0530

    hashed password logic

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const { User } = require('../models/user');
+const bcrypt=require('bcrypt')
 const router=express.Router()
 
 router.get('/',async (req,res)=>{
@@ -10,14 +11,27 @@ router.get('/',async (req,res)=>{
     res.send(userList);
 })
 
+router.get('/:id',async(req,res)=>{
+    const id=req.params.id;
+    try {
+        const user=await User.findById(id);
+        if(!user){
+            return res.status(404).json({message:"Category not found"})
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 router.post('/',async(req,res)=>{
     const data=req.body;
-    
+    const hashPassword=await bcrypt.hash(req.body.password,10);
     try {
         const user=new User({
             name:data.name,
             email:data.email,
-            passwordHash:data.passwordHash,
+            passwordHash:hashPassword,
             phone: data.phone,
             isAdmin: data.isAdmin,
             street:data.street,
