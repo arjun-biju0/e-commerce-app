dc8288d65a1cb9d75a530eb6379b623f67c11786
Arjun <bijuarjun45@gmail.com>
2025-02-07 11:58:21 +0530

    m

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,11 +4,24 @@ const { OrderItem } = require('../models/order-item');
 const router=express.Router();
 
 router.get('/',async (req,res)=>{
-    const orderList=await Order.find();
+    const orderList=await Order.find().populate('user', 'name');
     if(!orderList){
         res.status(500).json({success:false})
     }
     res.send(orderList);
+})
+
+router.get('/:id',async (req,res)=>{
+    try {
+        const order=await Order.findById(req.params.id).populate('user', 'name');
+        if(!order){
+            return res.status(400).json({message:"Order does not exist"})
+        }
+        return res.status(200).send(order);
+        
+    } catch (error) {
+       res.status(500).json({error}) 
+    }
 })
 
 router.post('/',async(req,res)=>{
@@ -43,6 +56,50 @@ router.post('/',async(req,res)=>{
     } catch (error) {
         console.error(error);
         res.status(500).json({success:false,error:error.message})
+    }
+})
+
+router.put('/:id',async (req,res)=>{
+    try {
+        const order=await Order.findByIdAndUpdate(req.params.id,{
+            status: req.body.status
+            },
+            {new: true}
+        )
+        if(!order){
+            return res.status(400).send("Order cannot be updated")
+        }
+        return res.status(200).json({order})
+        
+    } catch (error) {
+        res.status(500).json({error})
+    }
+})
+
+router.delete('/:id',async (req,res)=>{
+    try {
+        const result =await Order.findByIdAndDelete(req.params.id)
+        if(result){
+            
+            console.log(result.orderItems);
+            const result2=result.orderItems.map(async (orderItemId)=>{
+                const result2= await OrderItem.findByIdAndDelete(orderItemId)
+                return result2
+            })
+            if(result2){
+                return res.status(200).json({
+                    success: true,
+                    message:"Order deleted successfully"
+                })
+
+            }
+
+        }
+
+        return res.status(404).json({success:false, message:"Order not found"})
+        
+    } catch (error) {
+        res.status(500).json({error})
     }
 })
 
